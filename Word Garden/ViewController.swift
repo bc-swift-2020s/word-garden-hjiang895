0da8536173dbf16d0c2e32051e9ae4c7de202f96
Hannah Jiang <jiangbz@bc.edu>
2020-02-01 20:57:44 -0500

    3.2 - Updated user interface: show/hide keyboard, firstResponder, UITextField events

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -8,12 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessLetterField: UITextField!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        guessLetterField.resignFirstResponder()
+        updateUIAfterGuess()
+        
+    }
+    
+    func updateUIAfterGuess(){
+        guessLetterField.resignFirstResponder()
+        guessLetterField.text = ""
+    }
+    
+    @IBAction func guessLetterFieldChanged(_ sender: UITextField) {
+        print("guessLetterField changed!")
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        updateUIAfterGuess()
+        
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
 
 
 }
